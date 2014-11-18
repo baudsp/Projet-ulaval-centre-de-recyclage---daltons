eee229fad95a92c5d4b448320991b7b25b4ab9a7
kevinsalles <kvin.salles@gmail.com>
2014-11-17 22:13:08 -0500

    Mise a jour de la version finale

--- Code/RecyclApp/src/recyclapp/view/InterfaceOutils.java
+++ Code/RecyclApp/src/recyclapp/view/InterfaceOutils.java
@@ -4,18 +4,17 @@ package recyclapp.view;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.util.LinkedList;
 import javax.swing.JPanel;
 
-public class InterfaceOutils extends JPanel implements MouseListener {
+public class InterfaceOutils extends JPanel{
     private boolean moveTools;
     private int idTools;
     private int[][] coordImage;
     private int[] coord;
     private LinkedList<Image> images;
     private int sizeImage;
+    private int paddingTop;
 
     public InterfaceOutils() {
         this.idTools = -1;
@@ -29,13 +28,14 @@ public class InterfaceOutils extends JPanel implements MouseListener {
         images.add(getToolkit().getImage("resources/tool1.png"));
         images.add(getToolkit().getImage("resources/tool2.png"));
         sizeImage = 50;
+        
     }
     
     private int searchTool(int x, int y) {
         for(int i=0;i<2;i++){
-            if (x >= coordImage[i][0] && x <= coordImage[i][0] + sizeImage && y >= coordImage[i][1] && y <= coordImage[i][1] + sizeImage) {
+            if (x >= coordImage[i][0] && x <= coordImage[i][0] + sizeImage && y >= coordImage[i][1]  && y <= coordImage[i][1]  + sizeImage) {
                 coord[0] = coordImage[i][0];
-                coord[0] = coordImage[i][1];
+                coord[1] = coordImage[i][1];
                 return i;
             }
         }
@@ -55,8 +55,21 @@ public class InterfaceOutils extends JPanel implements MouseListener {
         }
     }
     
+    public int getIdTools() {
+        return idTools;
+    }
+    
+    public void setMoveTools(boolean b)
+    {
+        this.moveTools = b;
+    }
+    
     public boolean isMoveTools() {
         return moveTools;
+    }
+    
+    public Image getImages(int i) {
+        return images.get(i);
     }
     
     @Override
@@ -70,33 +83,10 @@ public class InterfaceOutils extends JPanel implements MouseListener {
         g.drawImage(images.get(0), coordImage[0][0],coordImage[0][1],sizeImage,sizeImage, this);
         g.drawImage(images.get(1), coordImage[1][0],coordImage[1][1],sizeImage,sizeImage, this);
         g.setColor(Color.red);
-        g.drawRect(coord[0],coord[1],sizeImage,sizeImage);
-        g.setColor(Color.red);
-    }
-    
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(this.moveTools)
+            g.drawRect(coord[0],coord[1],sizeImage,sizeImage);
+        else
+            g.drawRect(0,0,0,0);
     }
     
 }
--- Code/RecyclApp/src/recyclapp/view/InterfacePrincipale.java
+++ Code/RecyclApp/src/recyclapp/view/InterfacePrincipale.java
@@ -5,10 +5,15 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class InterfacePrincipale extends javax.swing.JFrame implements ActionListener, MouseMotionListener, MouseListener {
 
-    private InterfaceOutils subPaneTools;
+    private InterfaceOutils panelTools;
+    private InterfacePlan  panelMap;
+    private ModeleInterfacePrincipal mip;
+    private JLabel jLabel1;
 
     public InterfacePrincipale() {
         initComponents();
@@ -18,14 +23,69 @@ public class InterfacePrincipale extends javax.swing.JFrame implements ActionLis
     private void initialize() {
         this.addMouseMotionListener(this);
         this.addMouseListener(this);
+        panelMap = new InterfacePlan ();
+        panelMap.setBackground(java.awt.Color.white);
+        javax.swing.GroupLayout panelMapLayout = new javax.swing.GroupLayout(panelMap);
+        panelMap.setLayout(panelMapLayout);
+        getContentPane().add(panelMap, java.awt.BorderLayout.CENTER);
         panelMap.addMouseMotionListener(this);
-        subPaneTools = new InterfaceOutils();
-        subPaneTools.setBackground(javax.swing.UIManager.getDefaults().getColor("CheckBoxMenuItem.selectionBackground"));
-        javax.swing.GroupLayout subPaneToolsLayout = new javax.swing.GroupLayout(subPaneTools);
-        subPaneTools.setLayout(subPaneToolsLayout);
-        panelTools.add(subPaneTools, java.awt.BorderLayout.CENTER);
-
+        jLabel1 = new javax.swing.JLabel();
+        jLabel1.setFont(new java.awt.Font("Waree", 1, 18));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Outils");
+        panelTools = new InterfaceOutils();
+        panelTools.add(jLabel1, java.awt.BorderLayout.PAGE_START);
+        panelTools = new InterfaceOutils();
+        panelTools.setBackground(javax.swing.UIManager.getDefaults().getColor("List.selectionBackground"));
+        panelTools.setPreferredSize(new java.awt.Dimension(150, 588));
+        panelTools.setLayout(new java.awt.BorderLayout());
+        panelTools.add(jLabel1, java.awt.BorderLayout.PAGE_START);
+        getContentPane().add(panelTools, java.awt.BorderLayout.LINE_START);
+        
+        mip = new ModeleInterfacePrincipal(this);
     }
+
+    public JLabel getLog() {
+        return log;
+    }
+
+    public void setLog(JLabel log) {
+        this.log = log;
+    }
+
+    public JPanel getPanelInfo() {
+        return panelInfo;
+    }
+
+    public void setPanelInfo(JPanel panelInfo) {
+        this.panelInfo = panelInfo;
+    }
+
+    public InterfacePlan  getPanelMap() {
+        return panelMap;
+    }
+
+    public void setPanelMap(InterfacePlan  panelMap) {
+        this.panelMap = panelMap;
+    }
+
+    public JPanel getPanelParam() {
+        return panelParam;
+    }
+
+    public void setPanelParam(JPanel panelParam) {
+        this.panelParam = panelParam;
+    }
+
+    public InterfaceOutils getPanelTools() {
+        return panelTools;
+    }
+
+    public void setPanelTools(InterfaceOutils panelTools) {
+        this.panelTools = panelTools;
+    }
+    
+    
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -42,23 +102,24 @@ public class InterfacePrincipale extends javax.swing.JFrame implements ActionLis
             System.out.print(e.getSource().toString());
             log.setText("[" + e.getX() + ";" + e.getY() + "]");
         }
-        subPaneTools.moveTool(e.getX(), e.getY() -100);
-        if (subPaneTools.isMoveTools()) {
-            //ui.changeCursor(-2);
+   
+        int top = this.jMenuBar1.getHeight() * 2;
+        panelTools.moveTool(e.getX(), e.getY()-top);
+        if (panelTools.isMoveTools()) {
+            mip.changeCursor(-2);
         } else {
-            //ui.changeCursor(-1);
+            mip.changeCursor(-1);
         }
 
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        /*if (jPanel1.isMoveTools()) {
+        if (this.panelTools.isMoveTools()) {
             System.out.print("Drag Outils \n");
-            ui.changeCursor(jPanel1.getIdTools());
-            this.jPanel1.repaint();
+            mip.changeCursor(this.panelTools.getIdTools());
+            this.panelTools.repaint();
         }
-        System.out.print("------ \n");*/
     }
 
     @Override
@@ -67,20 +128,20 @@ public class InterfacePrincipale extends javax.swing.JFrame implements ActionLis
 
     @Override
     public void mousePressed(MouseEvent e) {
-        //jPanel1.moveTool(e.getX(),e.getY()-this.getjButton1().getHeight());
+        this.panelTools.repaint();
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        /* ui.changeCursor(-1);
-        this.jPanel1.setCoord();
-        this.jPanel1.repaint();
-        if (this.jPanel1.getIdTools() >= 0) {
-            int x = e.getX() - this.jPanel1.getWidth() - 25 - 8;
-            int y = e.getY() - this.jButton1.getHeight() - 25 - 8;
-            Image img = jPanel1.getImages(jPanel1.getIdTools());
-            jPanel2.addElement(jPanel1.getIdTools(), x, y, jPanel1.getCoordW(jPanel1.getIdTools()), jPanel1.getCoordH(jPanel1.getIdTools()), jPanel1.getImages(jPanel1.getIdTools()));
-        }*/
+        mip.changeCursor(-1);
+        this.panelTools.setMoveTools(false);
+        this.panelTools.repaint();
+        if (this.panelTools.getIdTools() >= 0) {
+            //int x = e.getX() - this.panelTools.getWidth() - 25 - 8;
+            //int y = e.getY() - this.jButton1.getHeight() - 25 - 8;
+            //Image img = jPanel1.getImages(jPanel1.getIdTools());
+            //jPanel2.addElement(jPanel1.getIdTools(), x, y, jPanel1.getCoordW(jPanel1.getIdTools()), jPanel1.getCoordH(jPanel1.getIdTools()), jPanel1.getImages(jPanel1.getIdTools()));
+        }
     }
 
     @Override
@@ -95,31 +156,18 @@ public class InterfacePrincipale extends javax.swing.JFrame implements ActionLis
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        panelTools = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
         panelParam = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         panelInfo = new javax.swing.JPanel();
         log = new javax.swing.JLabel();
-        panelMap = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
         jMenu3 = new javax.swing.JMenu();
         jCheckBoxMenuItem1 = new javax.swing.JCheckBoxMenuItem();
+        itemGrid = new javax.swing.JCheckBoxMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        panelTools.setBackground(javax.swing.UIManager.getDefaults().getColor("List.selectionBackground"));
-        panelTools.setPreferredSize(new java.awt.Dimension(150, 588));
-        panelTools.setLayout(new java.awt.BorderLayout());
-
-        jLabel1.setFont(new java.awt.Font("Waree", 1, 18)); // NOI18N
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Outils");
-        panelTools.add(jLabel1, java.awt.BorderLayout.PAGE_START);
-
-        getContentPane().add(panelTools, java.awt.BorderLayout.LINE_START);
 
         panelParam.setBackground(javax.swing.UIManager.getDefaults().getColor("CheckBoxMenuItem.selectionBackground"));
         panelParam.setMaximumSize(new java.awt.Dimension(300, 32767));
@@ -145,21 +193,6 @@ public class InterfacePrincipale extends javax.swing.JFrame implements ActionLis
 
         getContentPane().add(panelInfo, java.awt.BorderLayout.PAGE_END);
 
-        panelMap.setBackground(java.awt.Color.white);
-
-        javax.swing.GroupLayout panelMapLayout = new javax.swing.GroupLayout(panelMap);
-        panelMap.setLayout(panelMapLayout);
-        panelMapLayout.setHorizontalGroup(
-            panelMapLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 719, Short.MAX_VALUE)
-        );
-        panelMapLayout.setVerticalGroup(
-            panelMapLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 588, Short.MAX_VALUE)
-        );
-
-        getContentPane().add(panelMap, java.awt.BorderLayout.CENTER);
-
         jMenuBar1.setBackground(new java.awt.Color(164, 183, 145));
 
         jMenu1.setText("File");
@@ -174,6 +207,15 @@ public class InterfacePrincipale extends javax.swing.JFrame implements ActionLis
         jCheckBoxMenuItem1.setText("Afficher le panneaux de paramètre");
         jMenu3.add(jCheckBoxMenuItem1);
 
+        itemGrid.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_G, java.awt.event.InputEvent.CTRL_MASK));
+        itemGrid.setText("Afficher la grille");
+        itemGrid.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemGridActionPerformed(evt);
+            }
+        });
+        jMenu3.add(itemGrid);
+
         jMenuBar1.add(jMenu3);
 
         setJMenuBar(jMenuBar1);
@@ -181,10 +223,15 @@ public class InterfacePrincipale extends javax.swing.JFrame implements ActionLis
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void itemGridActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemGridActionPerformed
+        
+        this.mip.GridView();
+    }//GEN-LAST:event_itemGridActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBoxMenuItem itemGrid;
     private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem1;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
@@ -192,8 +239,7 @@ public class InterfacePrincipale extends javax.swing.JFrame implements ActionLis
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JLabel log;
     private javax.swing.JPanel panelInfo;
-    private javax.swing.JPanel panelMap;
     private javax.swing.JPanel panelParam;
-    private javax.swing.JPanel panelTools;
     // End of variables declaration//GEN-END:variables
+
 }
